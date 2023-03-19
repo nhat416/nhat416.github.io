b4cec8f01acaa013a0714e1304c0a7e6de779efe
Nhat Tran <nhatt416@gmail.com>
2023-03-19 11:50:39 -0400

    files

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Nhat Tran - Cloud | Apps | DevOps';
+export const SITE_DESCRIPTION = 'This is the portoflio page for Nhat Tran - a cloud and DevOps engineer and software developer based in Toronto, Canada.';
